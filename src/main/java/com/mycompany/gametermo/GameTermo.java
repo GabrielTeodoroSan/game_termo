197c183ada7f15583e7f279bb3bcedda9a9c14a1
= <=>
2025-06-28 07:49:30 -0300

    codigo

---
+++ src/main/java/com/mycompany/gametermo/GameTermo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gametermo;
+
+/**
+ *
+ * @author MASTER
+ */
+public class GameTermo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
